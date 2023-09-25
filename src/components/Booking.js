549b0b364124ccfd2b5a0fce93a8c5a692903ca1
Christopher Maag <christopher.maag@outlook.com>
2023-09-25 15:34:23 -0700

    Done

---
+++ src/components/Booking.js
@@ -0,0 +1,13 @@
+// to fix props issues
+
+import React from "react";
+import BookingForm from "./BookingForm";
+
+const Booking = (props) => {
+    return (
+        <BookingForm availableTimes={props.availableTimes} dispatch={props.dispatch} SubmitForm={props.SubmitForm}/>
+
+    );
+};
+
+export default Booking;
---
+++ src/components/Header.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Header = () => {
+    return (
+    <header className= 'header'>
+        <section>
+            {/*banner texts*/}
+            <div className="banner">
+                <h2>Little Lemon</h2>
+                <h3>Chicago</h3>
+                <p>We are a family owned Meditteranean restuarant, focused on traditional recipes served with a modern twist.</p>
+                <Link to="/booking"><button aria-Label='On Click'>Reserve Table</button></Link>
+            </div>
+
+            {/* banner image*/}
+            <div className='banner-img'>
+                <img src={bannerImg} alt=''/>
+            </div>
+</section>
+
+
+    </header>
+    );
+};
+
+export default Header;
